97c05055ed5eb9ecc71b61f3f5c9768b4646e47c
villadaniel750 <villanuevaquelin@gmail.com>
2025-12-16 21:10:15 -0300

    mongodb connection ready for all enviroments

--- src/main/java/org/earnlumens/mediastore/web/controller/HealthCheckController.java
+++ src/main/java/org/earnlumens/mediastore/web/controller/HealthCheckController.java
@@ -1,6 +1,8 @@
 package org.earnlumens.mediastore.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +12,16 @@ public class HealthCheckController {
     @Value("${spring.profiles.active:NOT_SET}")
     private String activeProfile;
 
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
     @GetMapping("/public")
     public String getPublicContent() {
-        return "public content | profile=" + activeProfile;
+        try {
+            long count = mongoTemplate.getCollection("founders").countDocuments();
+            return "public content | profile=" + activeProfile + " | founders=" + count;
+        } catch (Exception e) {
+            return "public content | profile=" + activeProfile + " | db error: " + e.getMessage();
+        }
     }
 }
